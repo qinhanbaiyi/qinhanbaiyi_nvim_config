6882677a8c2e1e180414d873ac7a297cd6d69ea9
qinhanbaiyi <68527344+qinhanbaiyi@users.noreply.github.com>
2021-12-21 20:47:26 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,13 +1,29 @@
+-- -- VSCode Neovim
+-- vim.cmd({
+-- 	if exists('g:vscode')
+-- 		" VSCode extension
+-- 	else
+-- 		" ordinary neovim
+-- 	endif
+-- })
+
+
 require('plugins')
-require('keymappings')
-require('nvim_setting')
+require('keymappings-config')
+require('nvim_setting-config')
 require('nvim-cmp')
 require('lsp-settings')
 
+--[[ require('Markdown-preview') ]]
+require('texmagic-config')
 
+require('vim-xkbswitch-config')
+require('format-config')
 
-
-
-
-
-
+require('indent_blankline-config')
+require('Treesitter-config')
+require('Autopairs-config')
+require('Telescope-config')
+require('nvim-web-devicons-config')
+require('which-key-config')
+require('barbar-config')
