35cbc0bca277b1c02d0c618c0bd67363b407b45b
qinhanbaiyi <1511565505@qq.com>
2022-09-02 18:21:23 +0800

    Mason

--- lua/Mason/mason-lsp.lua
+++ lua/Mason/mason-lsp.lua
@@ -49,12 +49,12 @@ require("lspconfig").sumneko_lua.setup({
 		},
 	},
 })
-require("lspconfig").gopls.setup({})
-require("lspconfig").rust_analyzer.setup({
-	settings = {
-		["rust_analyzer"] = {},
-	},
-})
+-- require("lspconfig").gopls.setup({})
+-- require("lspconfig").rust_analyzer.setup({
+-- 	settings = {
+-- 		["rust_analyzer"] = {},
+-- 	},
+-- })
 require("rust-tools").setup({
 	-- The "server" property provided in rust-tools setup function are the
 	-- settings rust-tools will provide to lspconfig during init.            --
@@ -82,59 +82,10 @@ require("rust-tools").setup({
 		},
 	},
 })
--- lua
-
--- mason_lsp.on_server_ready(function(server)
--- 	-- Specify the default options which we'll use to setup all servers
--- 	local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- 	local opts = { capabilities = capabilities }
--- 	local name = server.name
--- 	-- return_opts
--- 	-- @param { any } name
--- 	local return_opts = function(name)
--- 		if name == "remark_ls" then
--- 			opts = vim.tbl_deep_extend("force", {
--- 				autocmd = false,
--- 			}, opts)
--- 		elseif name == "sumneko_lua" then
--- 			local opts = require("lua-dev").setup({
--- 				lspconfig = vim.tbl_deep_extend("force", server:get_default_options(), opts),
--- 				library = {
--- 					plugins = {
--- 						"nvim-treesitter",
--- 						"plenary.nvim",
--- 						"telescope.nvim",
--- 					},
--- 				},
--- 			})
--- 		elseif name == "rust_analyzer" then
--- 			require("rust-tools").setup({
--- 				-- The "server" property provided in rust-tools setup function are the
--- 				-- settings rust-tools will provide to lspconfig during init.            --
--- 				-- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
--- 				-- with the user's own settings (opts).
--- 				server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
--- 				tools = {
--- 					autoSetHints = true,
--- 					inlay_hints = {
--- 						show_parameter_hints = true,
--- 						show_variable_name = false,
--- 						highlight = "Comment",
--- 					},
--- 				},
--- 			})
--- 		else
--- 			local opts = nil
--- 		end
--- 		return opts
--- 	end
--- 	local server_opts = return_opts(name)
--- 	if server_opts == nil then
--- 		server:setup(opts)
--- 	else
--- 		server:setup(server_opts)
--- 	end
--- end)
---
--- -- Latex and Markdown LSP
--- vim.cmd([[ autocmd BufRead,BufNewFile *.org set filetype=org ]])
+-- Latex and Markdown LSP
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.org" },
+	callback = function()
+		vim.bo.filetype = "org"
+	end,
+})
